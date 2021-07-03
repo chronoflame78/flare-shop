5bbd49d236d2d5af5f790e45ecf6be6f4a25beb9
chronoflame78 <gaovanthet555@gmail.com>
2021-07-03 18:36:33 +0700

    add cart utils

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,7 +11,7 @@ import CartDropDown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link to="/">
             <Logo className="logo"/>
@@ -31,12 +31,15 @@ const Header = ({ currentUser }) => (
             }
             <CartIcon/>
         </div>
-        <CartDropDown/>
+        {
+            hidden ? null : <CartDropDown/>
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
