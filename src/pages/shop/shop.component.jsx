5440619f04ef0c4d283d0dbcc327483941c61603
chronoflame78 <gaovanthet555@gmail.com>
2021-07-09 22:42:24 +0700

    refactor

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Route } from 'react-router-dom';
 
-import SHOP_DATA from './shop.data';
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-import CollectionPreview from '../../components/preview-collection/collection-preview.component';
+import CollectionPage from "../collection/collection.component";
 
-class ShopPage extends React.Component {
-    constructor(props){
-        super(props);
 
-        this.state = {
-            collections : SHOP_DATA
-        }
-    }
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview}/>
+    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}/>
+  </div>
+);
 
-    render(){
-        const {collections} = this.state;
-        return <div className="shop-page">
-            {
-                collections.map( ({id, ...otherColectionProps }) => (
-                    <CollectionPreview key={id} {...otherColectionProps} />
-                ))
-            }
-            </div>
-    }
-}
 
 export default ShopPage;
