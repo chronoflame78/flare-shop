78b5a093a51b7d53e9d2b27e8035daf154ac34c3
chronoflame78 <gaovanthet555@gmail.com>
2021-07-23 22:35:26 +0700

    update collection

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
 
+import { selectCollection } from '../../redux/shop/shop.selector';
+
 import './collection.styles.scss';
 
-const CollectionPage = ({match}) => (
+const CollectionPage = ({ collection }) => (
     <div className="collection-page">
         <h2>Collection Page</h2>
     </div>
 )
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+    colection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
