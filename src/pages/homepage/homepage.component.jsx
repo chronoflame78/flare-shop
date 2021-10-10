cf652d36b00ec3eccb0bbd9fbada85d36af0a7d8
chronoflame78 <gaovanthet555@gmail.com>
2021-10-10 21:49:15 +0700

    implement styled-component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,10 +4,18 @@ import Directory from '../../components/directory/directory.component';
 
 import './homepage.styles.scss';
 
+import HomePageContainer from './homepage.styles';
+
+// const HomePage = () => (
+//     <div className="homepage">
+//         <Directory/>
+//     </div>
+// )
+
 const HomePage = () => (
-    <div className="homepage">
+    <HomePageContainer>
         <Directory/>
-    </div>
+    </HomePageContainer>
 )
 
 export default HomePage;
---
+++ src/pages/homepage/homepage.styles.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export default HomePageContainer;
+
+
